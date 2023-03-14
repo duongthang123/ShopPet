37f06bb1262b6449d17c6f5d79a9356b1623497d
duongthang123 <88389413+duongthang123@users.noreply.github.com>
2023-03-14 20:03:13 +0700

    upload 7

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+
+
 // toggle menu
 const menu__mobile = document.getElementById("menu__mobile");
 const list = document.getElementById("header__nav-list");
@@ -10,9 +12,26 @@ menu__mobile.addEventListener("click", function() {
     isToggleOn = !isToggleOn;
 })
 
+
 //hiển thị submenu
 const menuParent = document.querySelector(".header__nav-parent");
 
 menuParent.addEventListener("click", function(){
     list.classList.add("block");
 });
+
+
+// active category
+const categorys = document.querySelectorAll(".category__list .category__item_link");
+console.log(categorys);
+
+
+for(let i=0; i<categorys.length; i++) {
+    categorys[i].addEventListener("click", function(e){
+        e.preventDefault();
+        categorys.forEach((other) => {
+            other.classList.remove("category__item_link--active");
+        });
+        categorys[i].classList.add("category__item_link--active");
+    });
+}
